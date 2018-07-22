4b131a74ac22b253147fc61231ca5cadb147d0f5
shuyi3 <bugdx123@gmail.com>
2018-07-22 14:39:41 -0700

    Some fixing

--- js/game.js
+++ js/game.js
@@ -262,8 +262,18 @@ function createEvents() {
     return allEvents;
 }
 
-
 //Utils
+
+// device detection
+function isMobile() {
+    if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent)
+        || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
+        return true;
+    }
+    return false;
+}
+
+
 function getRandomArrayElements(arr, count) {
     let shuffled = arr.slice(0), i = arr.length, min = i - count, temp, index;
     while (i-- > min) {
@@ -290,14 +300,14 @@ class Player {
 }
 
 class Event {
-    constructor(id, name, img, line, posLine, negLine, startstage, type, subsequent, posEffects, negEffects, playerId) {
+    constructor(id, name, img, line, posLine, negLine, startStage, type, subsequent, posEffects, negEffects, playerId) {
         this.id = id;
         this.name = name;
         this.img = img;
         this.line = line;
         this.posLine = posLine;
         this.negLine = negLine;
-        this.startstage = startstage;
+        this.startStage = startStage;
         this.type = type;
         this.subsequent = subsequent;
         this.posEffects = posEffects;
@@ -313,14 +323,22 @@ const EventType = Object.freeze({
     SUBSEQUENT: "SUBSEQUENT"
 });
 
+class Effect {
+    constructor(eventId, type, attr, val) {
+        this.eventId = eventId;
+        this.attr = attr;
+        this.type = type;
+        this.val = val;
+    }
+}
 
 class EventV2 {
-    constructor(id, name, img, line, eventType, choice1, choice2) {
+    constructor(id, name, img, line, startStage, eventType, choice1, choice2, playerId) {
         this.id = id;
         this.name = name;
         this.img = img;
         this.line = line;
-        this.startstage = startstage;
+        this.startStage = startStage;
         this.eventType = eventType;
         this.choice1 = choice1;
         this.choice2 = choice2;
@@ -349,9 +367,7 @@ const BUFF = Object.freeze({
     FAKE: "FAKE" // FAKE BUFF
 });
 
-
-
-class Effect {
+class EffectV2 {
     constructor(effectId, effectType, callBack, subsequentId) {
         if (!effectType in EffectType) {
             throw Error("Invalid effectType: " + effectType);
@@ -364,9 +380,9 @@ class Effect {
 }
 
 // Usage
-const eventV2 = new EventV2(1, 'fakeName', 'fakeImg', 'dummy line', EventType.NORMAL,
+const eventV2 = new EventV2(1, 'fakeName', 'fakeImg', 'dummy line', 1, EventType.NORMAL,
     new Choice(1, "this is choice 1", 
-        new Effect(
+        new EffectV2(
             "effectId1", EffectType.COMPOSITE, function () { //function 作为回调
                 player.agility += 1;
                 player.intelligence -= 1;
@@ -374,7 +390,7 @@ const eventV2 = new EventV2(1, 'fakeName', 'fakeImg', 'dummy line', EventType.NO
             }
         ), null),
     new Choice(2, "this is choice 2",
-        new Effect(
+        new EffectV2(
             "effectId2", EffectType.RANDOM, () => {
                 player.gold += - 5 + 10 * Math.random(); // +-5 gold effect
             }
@@ -400,7 +416,7 @@ function loadAllEvents(rawEvents) {
     const events = {};
     for (let i = 0; i < rawEvents.length; i++) {
         const event = rawEvents[i];
-        const level = event.startstage;
+        const level = event.startStage;
         if (!(level in events)) {
             events[level] = [];
         }
@@ -441,6 +457,10 @@ function initModels() {
 
     currentEvents.push(eventMap[STAGE_IDS[currentLevel]]);
     currentEvents = currentEvents.concat(getEventsForLevel(currentLevel + 1));
+
+    console.log("getEventsForLevel:");
+    console.log(getEventsForLevel(currentLevel + 1)[0]);
+
     currentMaxPage = 0;
     currentPage = 0;
 }
@@ -644,6 +664,8 @@ function checkDead() {
 }
 
 function createPage(event) {
+    console.log("creating...");
+    console.log(event);
     if (event.type === 'normal') {
         return createEventPageDiv(event);
     } else if (event.type === 'stage') {
@@ -693,6 +715,8 @@ function createEventPageDiv(event) {
 
 function addPages(events, turn) {
     for (let i = 0; i < events.length; i++) {
+        console.log("addPages");
+        console.log(events[i]);
         addPage(events[i], turn);
     }
 }
@@ -4093,6 +4117,12 @@ $(window).ready(function () {
     //       $('.book-wrapper').next().fadeIn(2000);
     //     })
     // );
+    let width = 800;
+    let height = 575;
+    if (isMobile()) {
+        width = 600;
+        height = 431;
+    }
 
     $('.button1').click(
         function () {
@@ -4100,8 +4130,8 @@ $(window).ready(function () {
         });
     $('.pages').turn({
         duration: 1500,
-        width: 800,
-        height: 575,
+        width: width,
+        height: height,
         // acceleration: true,
         //  display: 'single',
         autoCenter: true,
